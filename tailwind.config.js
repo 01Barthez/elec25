21c9c53ed2bf347f9d5faa09b25ff296b2099336
01Barthez <kenwoubarthez@gmail.com>
2024-09-18 14:47:54 +0100

    Section Sponsors

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,17 @@ module.exports = {
       secondary: "#001135",
       white: "#fff",
     },
-    extend: {},
+    extend: {
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 18s linear infinite',
+    },
   },
   plugins: [],
+}
 }
